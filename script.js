1448f777d745ba2b6f018be3b80ec00226038665
saihomedecor <saihomedecorandgifts@gmail.com>
2025-07-29 15:19:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+AOS.init();
 
-function sharePage() {
-  if (navigator.share) {
-    navigator.share({
-      title: 'Deepak Mishra – Trusted Insurance Partner',
-      url: window.location.href
-    }).catch(console.error);
-  } else {
-    alert("Sharing not supported on this browser.");
+const headlineText = "Deepak Mishra – Trusted Insurance Partner";
+let i = 0;
+function typeWriter() {
+  if (i < headlineText.length) {
+    document.getElementById("headline").innerHTML += headlineText.charAt(i);
+    i++;
+    setTimeout(typeWriter, 100);
   }
+}
+typeWriter();
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
