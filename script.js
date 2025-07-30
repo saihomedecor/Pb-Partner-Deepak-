d2a30f8463e65e08f523efd0d255e30ba3046adf
saihomedecor <saihomedecorandgifts@gmail.com>
2025-07-30 21:08:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-// Scroll To Top Button
-const scrollTopBtn = document.getElementById("scrollTopBtn");
-window.onscroll = function () {
-  scrollTopBtn.style.display = window.scrollY > 200 ? "block" : "none";
-};
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-}
+AOS.init();
 
-// Counter Animation
-const counters = document.querySelectorAll('.counter');
-counters.forEach(counter => {
-  counter.innerText = '0';
-  const updateCounter = () => {
-    const target = +counter.getAttribute('data-target');
-    const count = +counter.innerText;
-    const increment = target / 100;
-    if (count < target) {
-      counter.innerText = `${Math.ceil(count + increment)}`;
-      setTimeout(updateCounter, 30);
-    } else {
-      counter.innerText = target;
-    }
-  };
-  updateCounter();
-});
+const headlineText = "Deepak Mishra – Trusted Insurance Partner";
+let i = 0;
+function typeWriter() {
+  if (i < headlineText.length) {
+    document.getElementById("headline").innerHTML += headlineText.charAt(i);
+    i++;
+    setTimeout(typeWriter, 100);
+  }
+}
+typeWriter();
+
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
